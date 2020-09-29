5780e494ad074af7f8ae9e87778afb0389ad3051
Seth Lutske <seth.lutske@gmail.com>
2020-09-29 17:34:54 +0000

    restructing scripts and plugins

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import EsriLeafletLayer from './EsriLeafletLayer';
-import EsriLeafletGeoSearch from './GeoSearch';
 import BasemapLayer from './BasemapLayer';
 import FeatureLayer from './FeatureLayer';
 import ImageMapLayer from './ImageMapLayer';
 import DynamicMapLayer from './DynamicMapLayer';
 import TiledMapLayer from './TiledMapLayer';
-import HeatmapLayer from './HeatmapLayer';
-import ClusterLayer from './ClusterLayer';
 
 export {
-	EsriLeafletLayer,
-	EsriLeafletGeoSearch,
-	BasemapLayer,
-	FeatureLayer,
-	ImageMapLayer,
-	DynamicMapLayer,
-	TiledMapLayer,
-	HeatmapLayer,
-	ClusterLayer,
+   EsriLeafletLayer,
+   BasemapLayer,
+   FeatureLayer,
+   ImageMapLayer,
+   DynamicMapLayer,
+   TiledMapLayer,
 };

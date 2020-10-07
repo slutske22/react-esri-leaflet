4a28b3d533b575912758c23557211b0029d9b290
slutske22 <seth.lutske@gmail.com>
2020-10-07 13:30:52 -0700

    restructuring v2 components folder

---
+++ src/v2/index.js
@@ -0,0 +1,15 @@
+import EsriLeafletLayer from './EsriLeafletLayer';
+import BasemapLayer from './BasemapLayer';
+import FeatureLayer from './FeatureLayer';
+import ImageMapLayer from './ImageMapLayer';
+import DynamicMapLayer from './DynamicMapLayer';
+import TiledMapLayer from './TiledMapLayer';
+
+export {
+   EsriLeafletLayer,
+   BasemapLayer,
+   FeatureLayer,
+   ImageMapLayer,
+   DynamicMapLayer,
+   TiledMapLayer,
+};

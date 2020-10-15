026ffb1e2a5012f5a76b89b93782e15bc4502559
slutske22 <seth.lutske@gmail.com>
2020-10-14 21:47:51 -0700

    setting up testing

---
+++ __tests__/BasemapLayer.js
@@ -0,0 +1,11 @@
+// import * as EL from 'esri-leaflet';
+import React from 'react';
+// import { renderIntoDocument } from 'react-dom/test-utils';
+
+// import { BasemapLayer } from '../src/BasemapLayer';
+
+describe('BasemapLayer', () => {
+	it('adds a esri-leaflet basemap to the map', () => {
+		console.log('inside basemap test');
+	});
+});

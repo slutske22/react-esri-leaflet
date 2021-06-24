bfece22d786b8556fedfe0ca9d5e2dd17f3c24f9
slutske22 <seth.lutske@gmail.com>
2021-06-23 22:06:42 -0700

    Importig leaflet css directly in example

--- examples/js/src/index.js
+++ examples/js/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+	iconUrl: icon,
+	shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(

f95bcb2870e70250be06fc6d4b104db7f20b120c
Seth Lutske <seth.lutske@gmail.com>
2022-04-21 07:39:21 -0700

    Initial round of cleanup

--- src-v2/DynamicMapLayer.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { DynamicMapLayerOptions } from 'esri-leaflet';
-import Layer from './EsriLeafletLayer';
-
-const DynamicMapLayer: React.FC<DynamicMapLayerOptions> = (
-	props: DynamicMapLayerOptions
-) => <Layer layerType="dynamicMapLayer" {...props} />;
-
-export default DynamicMapLayer;
--- src-v2/TiledMapLayer.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { TiledMapLayerOptions } from 'esri-leaflet';
-import Layer from './EsriLeafletLayer';
-
-const TiledMapLayer: React.FC<TiledMapLayerOptions> = (
-	props: TiledMapLayerOptions
-) => <Layer layerType="tiledMapLayer" {...props} />;
-
-export default TiledMapLayer;

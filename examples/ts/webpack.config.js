b0e001b20fb78f80d49738c22d96c9db02f5a3bf
Seth Lutske <seth.lutske@gmail.com>
2022-04-22 12:28:12 -0700

    Creating dist

--- examples/ts/webpack.config.js
+++ examples/ts/webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
   resolve: {

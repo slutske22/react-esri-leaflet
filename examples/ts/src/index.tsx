94336c3c059bd589019deb6db21f357fc73984a4
Seth Lutske <seth.lutske@gmail.com>
2025-06-29 06:49:00 -1000

    Small change

--- examples/ts/src/index.tsx
+++ examples/ts/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById("root") as HTMLElement;
 
 const root = ReactDOMClient.createRoot(rootElement);
 
